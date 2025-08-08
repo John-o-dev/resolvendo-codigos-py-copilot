f8d56fd09204eccb5cd5c56319f4dd3b12b5f909
Johnny Developer <86370332+John-o-dev@users.noreply.github.com>
2025-08-08 04:16:27 +0000

    resolvendo desafio de código

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,8 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+input1 = input("Insira a primeira informação: ")
+input2 = input("Insira a segunda informação: ")
+
+varConcat = input1 + " " + input2;
+
+print("Informação concatenada: ", varConcat)
--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,8 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+valor1 = input("Digite um valor: ")
+valor2 = int(input("Digite mais um valor: "))
+
+soma = int(valor1) + valor2
+
+print("TOTAL: ", soma)
